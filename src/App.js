93ecaa177ba213519d5f8ea4bfc029331aa51b01
Nikita <romanovnnikita@gmail.com>
2024-09-05 23:05:40 +0300

    финальный коммит

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { v4 as uuidv4 } from "uuid";
+import { useState } from "react";
+import Todoform from "./components/Todos/TodoForm";
+import TodoList from "./components/Todos/TodoList";
+import TodosActions from "./components/Todos/TodosActions";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodoHandler = (text) => {
+    const newTodo = {
+      text,
+      isCompleted: false,
+      id: uuidv4(),
+    };
+
+    setTodos([...todos, newTodo]);
+  };
+
+  const deleteTodoHandler = (id) => {
+    setTodos(
+      todos.filter((todo) => {
+        return todo.id !== id;
+      })
+    );
+  };
+
+  const toggleTodoHandler = (id) => {
+    setTodos(
+      todos.map((todo) => {
+        return todo.id === id
+          ? { ...todo, isCompleted: !todo.isCompleted }
+          : { ...todo };
+      })
+    );
+  };
+
+  const resetTodosHandler = () => {
+    setTodos([]);
+  };
+
+  const deleteCompletedTodosHandler = () => {
+    setTodos(
+      todos.filter((todo) => {
+        return todo.isCompleted === false;
+      })
+    );
+  };
+
+  const completedTodoCount = todos.filter((todo) => {
+    return todo.isCompleted;
+  }).length;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo App</h1>
+      <Todoform addTodo={addTodoHandler} />
+
+      {todos.length !== 0 && (
+        <TodosActions
+          resetTodos={resetTodosHandler}
+          deleteCompletedTodos={deleteCompletedTodosHandler}
+          completedTodosExist={!!completedTodoCount}
+        />
+      )}
+
+      <TodoList
+        todos={todos}
+        deleteTodo={deleteTodoHandler}
+        toggleTodo={toggleTodoHandler}
+      />
+
+      {completedTodoCount > 0 && (
+        <h1>{`You have completed ${completedTodoCount} ${
+          completedTodoCount < 2 ? "todo" : "todos"
+        }`}</h1>
+      )}
     </div>
   );
 }
